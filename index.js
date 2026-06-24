6819aee07a7c525ad758ed2936a6513a9e312d91
sownlee2 <nbin9941@gmail.com>
2026-06-25 01:20:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,18 +23,15 @@ function initBot() {
         console.log('Bot đã vào server thành công!');
     });
 
-    // Tự động đọc tin nhắn của server để gõ lệnh đăng nhập/đăng ký chuẩn xác
     bot.on('message', (jsonMsg) => {
         const message = jsonMsg.toString();
         
-        // Nếu server yêu cầu đăng ký tài khoản mới (lần đầu vào)
         if (message.includes('/register')) {
             setTimeout(() => {
                 bot.chat('/register sonbot123 sonbot123');
             }, 2000);
         }
         
-        // Nếu server yêu cầu đăng nhập (từ lần thứ 2 trở đi)
         if (message.includes('/login')) {
             setTimeout(() => {
                 bot.chat('/login sonbot123');
